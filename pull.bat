729dd29c045a1eadd48a6fe60af0e8246a14c39f
shs <ssh.spb@gmail.com>
2018-03-13 03:39:57 +0300

    add option --no-save comand npm install in pull.bat

--- pull.bat
+++ pull.bat
@@ -1,11 +1,15 @@
+REM переместимся в папку с программой
+d:
+cd \test\edumtu
+
 REM Спрятать локальные изменения в закладку
 git stash
 
-REM Удалить записи закладок
+REM Удалить закладку
 git stash clear
 
 REM извлечь данные с сервера и объединить
 git pull --no-edit origin master
 
 REM проверить по package.json наличие пакетов
-npm install
+npm install --no-save
