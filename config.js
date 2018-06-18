b580a0d1d4a071baee781bb4d1e5b87e6f453eba
shs <ssh.spb@gmail.com>
2018-06-18 21:35:55 +0300

    lyrics

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config_local = require('./config_local');
 module.exports = {
-  "version": "edumtu v1.9.9 08.06.2018",
+  "version": "edumtu v1.10.0 19.06.2018",
   "scope_list": ["Основная", "Наука"],
   "variant_list": ["Вариант: План", "Вариант: Факт"],
   "estimate_index": [
